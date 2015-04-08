681159f670acc1517e3a0c85c623009496eaad68
Sean <iceman198@gmail.com>
2015-04-08 15:37:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
-function StepperDriver(hardware, speed, posCount, debug, callback) {
+function StepperDriver(hardware, speed, debug, callback) {
 	var self = this;
 	
 	this.hardware = hardware;
 	this.speed = speed;
-	this.posCur = 0;
-	this.posCount = posCount - 1;
 	this.moveInt;
+	this.posCur = 0;
 	this.pinPulse = hardware.pin['G2'];
-	this.pinDir = hardware.pin['G1']; 
+	this.pinDir = hardware.pin['G3']; 
 	
 	if (debug) { this.debug = debug; }
 	else { this.debug = false; }
@@ -22,47 +21,33 @@ function StepperDriver(hardware, speed, posCount, debug, callback) {
 
 util.inherits(StepperDriver, EventEmitter);
 
-StepperDriver.prototype.moveTo = function(position) {
+StepperDriver.prototype.moveTo = function(position, direction) {
 	var self = this;
+	direction = parseFloat(direction);
 	clearInterval(self.moveInt);
 	self.pinPulse.output(0);
+	self.posCur = 0; // keep track of how far we've moved
 	
 	// 0 = counter clockwise / 1 = clockwise
-	if (position > self.posCur) { // move clockwise
-		self.pinDir.output(1);
-		self.moveInt = setInterval( function() {
-			if (self.posCur >= position || self.posCur > self.posCount) {
-				clearInterval(self.moveInt);
-				self.pinPulse.output(0);
-				setImmediate(function() {self.emit('arrived');}.bind(this));
-			}
-			else {
-				self.posCur += 1;
-				self.pinPulse.toggle();
-				if (self.debug) { console.log('StepperDriver~at position: ' + self.posCur); }
-			}
-		}.bind(this), self.speed);
-	}
+	if (direction == 0) { self.pinDir.output(0); }
+	else { self.pinDir.output(1); }
 	
-	else if (position < self.posCur) {
-		self.pinDir.output(0);
-		self.moveInt = setInterval( function() {
-			if (self.posCur <= position || self.posCur < 1) {
-				clearInterval(self.moveInt);
-				self.pinPulse.output(0);
-				setImmediate(function() {self.emit('arrived');}.bind(this));
-			}
-			else {
-				self.posCur -= 1;
-				self.pinPulse.toggle();
-				if (self.debug) { console.log('StepperDriver~at position: ' + self.posCur); }
-			}
-		}.bind(this), self.speed);
-	}
+	self.moveInt = setInterval( function() {
+		if (self.posCur >= position) {
+			clearInterval(self.moveInt);
+			self.pinPulse.output(0);
+			setImmediate(function() {self.emit('arrived');}.bind(this));
+		}
+		else {
+			self.posCur += 1;
+			self.pinPulse.toggle();
+			if (self.debug) { console.log('StepperDriver~at position: ' + self.posCur); }
+		}
+	}.bind(this), self.speed);
 }
 
-function use(hardware, speed, posCount, debug, callback) {
-	return new StepperDriver(hardware, speed, posCount, debug, callback);
+function use(hardware, speed, debug, callback) {
+	return new StepperDriver(hardware, speed, debug, callback);
 }
 
 exports.StepperDriver = StepperDriver;
